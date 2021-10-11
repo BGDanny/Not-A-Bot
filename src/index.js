239c2baa4b8cc78fe9e0b6a2338047bf0aa5c5fb
Junhao Xue <junhao.xue@justeattakeaway.com>
2021-10-11 11:54:46 -0600

    Add env var

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import fetch from 'node-fetch';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 fetch("https://zenquotes.io/api/random")
     .then(res => res.json())
     // .then(res => JSON.parse(res))
     .then(data => console.log(data))
     .catch(e => console.error(e));
+console.log(process.env['TOKEN']);
